038dff78cdabe4cfe86c117e45c72b9e145d6874
leowang <leowang9863@gmail.com>
2018-12-04 17:18:17 -0500

    made huge progress

--- index.js
+++ index.js
@@ -1,29 +1,44 @@
 const Jimp = require('Jimp')
+const fs = require('fs')
 var Express = require('express');
 var multer = require('multer');
 var bodyParser = require('body-parser');
 var app = Express();
+var ejs = require('ejs')
+app.use(Express.static('public'));
 app.use(bodyParser.json());
+app.set('views', __dirname + '/public')
+app.set('view engine', 'ejs')
 
-var image2 = new Jimp('image/screen.PNG', function (err, img) {
-    err ? console.log('logo err' + err) : console.log('logo created and ready for use');
-    return img.scale(2.5);
+app.listen(3000, function (a) {
+    console.log("Listening to port 3000");
 });
+
+// var image2 = new Jimp('image/screen.PNG', function (err, img) {
+//     err ? console.log('logo err' + err) : console.log('logo created and ready for use');
+//     return img.scale(2.5);
+// });
 
 function insert(screenshot) {
 
-    Jimp.read('image/Frame.PNG')
+    Jimp.read('public/image/Frame.PNG')
 
         .then(image => {
             image.clone()
 
                 .composite(screenshot, 320, 580)
-                .write('image/full.jpg')
+                .write('public/image/full.jpg')
         })
         .catch(err => {
             console.error(err);
         });
 }
+
+function update(res){
+    action = true;
+    res.redirect('/');
+}
+
 // const promise = require('make-promises-safe')
 // const mergeImages = require('merge-images');
 // const Canvas = require('canvas');
@@ -35,11 +50,12 @@ function insert(screenshot) {
 //   .catch(err => {
 //     console.error(err);
 // });// data:image/png;base64,iVBORw0KGgoAA...
-
+var imageName = "image/full.jpg"
+var action = false
 
 var Storage = multer.diskStorage({
     destination: function (req, file, callback) {
-        callback(null, "image");
+        callback(null, "public/image");
     },
     filename: function (req, file, callback) {
         callback(null, file.originalname);
@@ -51,7 +67,10 @@ var upload = multer({
 }).array("imgUploader", 3); //Field name and max count
 
 app.get("/", function (req, res) {
-    res.sendFile(__dirname + "/index.html");
+    res.render('index',{
+        action: action,
+        // fileName: imageName
+    });
 });
 
 app.post("/api/Upload", function (req, res) {
@@ -59,15 +78,42 @@ app.post("/api/Upload", function (req, res) {
         if (err) {
             return res.end("Something went wrong!");
         }
-        return res.end("File uploaded sucessfully!.");
+        var screen = new Jimp('public/image/screen1.PNG', function (err, img) {
+            err ? console.log('logo err' + err) : console.log('logo created and ready for use');
+            return img.scale(2.5);
+        });
+        Jimp.read('public/image/Frame.PNG')
+
+        .then(image => {
+            image.clone()
+
+                .composite(screen, 320, 580)
+                .write('public/image/full.jpg')
+        })
+        .catch(err => {
+            console.error(err);
+        });
+        setTimeout(function(){
+            action = true;
+            res.redirect('/');
+        }, 3000);
+        
     });
-    var screen = new Jimp('image/screen1.PNG', function (err, img) {
-        err ? console.log('logo err' + err) : console.log('logo created and ready for use');
-        return img.scale(2.5);
-    });
-    insert(screen);
 });
 
-app.listen(3000, function (a) {
-    console.log("Listening to port 3000");
-});
+
+
+// app.get("/api/Upload", function (req, res){
+//     res.render('index',{
+//         action: action,
+//         fileName: imageName
+//     });
+// })
+
+
+
+
+
+
+
+
